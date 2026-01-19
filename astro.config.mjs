5c752f6e595b11cc70303264cb3dea58ed5a6f17
adel mokrani <gamerdz525@gmail.com>
2026-01-19 10:25:35 +0300

    Add Dockerfile for Coolify deployment and remove Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://texblog.ru/',
   output: 'static',
-  adapter: vercel({
-    imageService: true
-  }),
   integrations: [tailwind(), sitemap()],
   trailingSlash: 'never',
   compressHTML: true,
